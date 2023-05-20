1df17c5d27dcc0a7756a3d7f087b522196a4d5a2
Johan Martinez <johan.crossm@gmail.com>
2023-05-20 15:12:54 -0500

    Adding Numbers

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World y un chanchito Feliz!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+    private void sum(int a, int b, int c){
+        int result = a + b + c;
     }
 }
